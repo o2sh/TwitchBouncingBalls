1cf95c8b12f2c0adb1e9f102fba88b38b24d4f38
Ossama Hjaji <ossama-hjaji@live.fr>
2018-08-12 02:42:39 +0200

    submit function handled

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ import (
 	twitch "github.com/proletariatgames/go-twitch/twitch"
 )
 
+//Docker fix
+const (
+	MethodGet    = "GET"
+	MethodPost   = "POST"
+	MethodPut    = "PUT"
+	MethodDelete = "DELETE"
+)
+
 //Output data send to the client
 type Output struct {
 	Total   int
@@ -16,6 +24,7 @@ type Output struct {
 }
 
 var tpl *template.Template
+var client *twitch.Client
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.html"))
@@ -25,36 +34,39 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "5000"
+		port = "8000"
 	}
 
+	os.Setenv("GO_TWITCH_CLIENTID", "5hk0e3wz856a198lypq5bai57nf13u")
+	client = twitch.NewClient(&http.Client{})
 	http.HandleFunc("/", idx)
+	http.HandleFunc("/submit", sub)
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("pub"))))
 	log.Printf("Listening on port %s\n\n", port)
 	http.ListenAndServe(":"+port, nil)
 }
 
 func idx(w http.ResponseWriter, req *http.Request) {
-	total := 20
 	var err error
-	os.Setenv("GO_TWITCH_CLIENTID", "5hk0e3wz856a198lypq5bai57nf13u")
 
-	client := twitch.NewClient(&http.Client{})
+	out := getStreams("programming")
 
-	opt := &twitch.ListOptions{
-		Game:  "programming",
-		Limit: total,
+	err = tpl.ExecuteTemplate(w, "index.html", out)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
 	}
+}
 
-	sd, err := client.Streams.List(opt)
-	check(err)
-	//total = sd.Total
-	/*for _, stream := range sd.Streams {
-		channels = append(channels, stream.Channel)
-	}*/
+func sub(w http.ResponseWriter, req *http.Request) {
+	var game string
+	var err error
 
-	out := Output{total, sd.Streams}
-
+	if req.Method == MethodPost {
+		game = req.FormValue("game")
+	}
+	out := getStreams(game)
 	err = tpl.ExecuteTemplate(w, "index.html", out)
 	if err != nil {
 		log.Println(err)
@@ -67,4 +79,19 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func getStreams(game string) Output {
+	total := 20
+
+	opt := &twitch.ListOptions{
+		Game:  game,
+		Limit: total,
+	}
+
+	sd, err := client.Streams.List(opt)
+	check(err)
+
+	out := Output{total, sd.Streams}
+	return out
 }
